7f6d633f595cb582fec9bfc895f76a8d8d893b7b
fkof <jesus120689@gmail.com>
2014-12-04 14:55:35 -0600

    Conexion
    Se creo la conexion al servidor.

--- CompleteOP/CompleteOP/Form1.Designer.vb
+++ CompleteOP/CompleteOP/Form1.Designer.vb
@@ -22,82 +22,205 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TabControl1 = New System.Windows.Forms.TabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.TabControl1.SuspendLayout()
-        Me.TabPage1.SuspendLayout()
+        Me.gbdatos = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtServidor = New System.Windows.Forms.TextBox()
+        Me.txtusuario = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtpassword = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.btnprocess = New System.Windows.Forms.Button()
+        Me.txtbd = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.dtfecha = New System.Windows.Forms.DateTimePicker()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.gbdatos.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'TabControl1
+        'gbdatos
         '
-        Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
-        Me.TabControl1.Location = New System.Drawing.Point(12, 51)
-        Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(688, 338)
-        Me.TabControl1.TabIndex = 0
+        Me.gbdatos.Controls.Add(Me.Label7)
+        Me.gbdatos.Controls.Add(Me.dtfecha)
+        Me.gbdatos.Controls.Add(Me.txtbd)
+        Me.gbdatos.Controls.Add(Me.Label4)
+        Me.gbdatos.Controls.Add(Me.btnprocess)
+        Me.gbdatos.Controls.Add(Me.txtpassword)
+        Me.gbdatos.Controls.Add(Me.Label3)
+        Me.gbdatos.Controls.Add(Me.txtusuario)
+        Me.gbdatos.Controls.Add(Me.Label2)
+        Me.gbdatos.Controls.Add(Me.txtServidor)
+        Me.gbdatos.Controls.Add(Me.Label1)
+        Me.gbdatos.Location = New System.Drawing.Point(12, 12)
+        Me.gbdatos.Name = "gbdatos"
+        Me.gbdatos.Size = New System.Drawing.Size(358, 310)
+        Me.gbdatos.TabIndex = 0
+        Me.gbdatos.TabStop = False
+        Me.gbdatos.Text = "GroupBox1"
         '
-        'TabPage1
+        'Label1
         '
-        Me.TabPage1.Controls.Add(Me.GroupBox2)
-        Me.TabPage1.Controls.Add(Me.GroupBox1)
-        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(680, 312)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "TabPage1"
-        Me.TabPage1.UseVisualStyleBackColor = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Servidor"
         '
-        'TabPage2
+        'txtServidor
         '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(472, 304)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
-        Me.TabPage2.UseVisualStyleBackColor = True
+        Me.txtServidor.Location = New System.Drawing.Point(88, 23)
+        Me.txtServidor.Name = "txtServidor"
+        Me.txtServidor.Size = New System.Drawing.Size(192, 20)
+        Me.txtServidor.TabIndex = 1
         '
-        'GroupBox1
+        'txtusuario
         '
-        Me.GroupBox1.Location = New System.Drawing.Point(6, 6)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(325, 247)
-        Me.GroupBox1.TabIndex = 0
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
+        Me.txtusuario.Location = New System.Drawing.Point(88, 66)
+        Me.txtusuario.Name = "txtusuario"
+        Me.txtusuario.Size = New System.Drawing.Size(192, 20)
+        Me.txtusuario.TabIndex = 3
         '
-        'GroupBox2
+        'Label2
         '
-        Me.GroupBox2.Location = New System.Drawing.Point(337, 6)
-        Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(325, 247)
-        Me.GroupBox2.TabIndex = 1
-        Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "GroupBox2"
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Usuario"
+        '
+        'txtpassword
+        '
+        Me.txtpassword.Location = New System.Drawing.Point(88, 106)
+        Me.txtpassword.Name = "txtpassword"
+        Me.txtpassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtpassword.Size = New System.Drawing.Size(192, 20)
+        Me.txtpassword.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 113)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Password"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(637, 35)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(425, 287)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'btnprocess
+        '
+        Me.btnprocess.Location = New System.Drawing.Point(277, 265)
+        Me.btnprocess.Name = "btnprocess"
+        Me.btnprocess.Size = New System.Drawing.Size(75, 39)
+        Me.btnprocess.TabIndex = 6
+        Me.btnprocess.Text = "Button1"
+        Me.btnprocess.UseVisualStyleBackColor = True
+        '
+        'txtbd
+        '
+        Me.txtbd.Location = New System.Drawing.Point(88, 145)
+        Me.txtbd.Name = "txtbd"
+        Me.txtbd.Size = New System.Drawing.Size(192, 20)
+        Me.txtbd.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 152)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Base de Datos"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(377, 35)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(254, 287)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(376, 19)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(72, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Sps Alterados"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(634, 19)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(51, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Complete"
+        '
+        'dtfecha
+        '
+        Me.dtfecha.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtfecha.Location = New System.Drawing.Point(114, 191)
+        Me.dtfecha.Name = "dtfecha"
+        Me.dtfecha.Size = New System.Drawing.Size(166, 20)
+        Me.dtfecha.TabIndex = 9
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(6, 198)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(102, 13)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Crear hasta la fecha"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(712, 401)
-        Me.Controls.Add(Me.TabControl1)
+        Me.ClientSize = New System.Drawing.Size(1088, 334)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.gbdatos)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        Me.TabControl1.ResumeLayout(False)
-        Me.TabPage1.ResumeLayout(False)
+        Me.gbdatos.ResumeLayout(False)
+        Me.gbdatos.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
-    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
-    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
-    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
-    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents gbdatos As System.Windows.Forms.GroupBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents dtfecha As System.Windows.Forms.DateTimePicker
+    Friend WithEvents txtbd As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents btnprocess As System.Windows.Forms.Button
+    Friend WithEvents txtpassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtusuario As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtServidor As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
 
 End Class
--- CompleteOP/CompleteOP/Form1.vb
+++ CompleteOP/CompleteOP/Form1.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Form1
 
+    Private Sub btnprocess_Click(sender As System.Object, e As System.EventArgs) Handles btnprocess.Click
+        Dim Cnn As New SqlClient.SqlConnection
+        Cnn.ConnectionString = String.Format("Server={0};Database={1}; User Id={2}; Password={3};", txtServidor.Text, txtbd.Text, txtusuario.Text, txtpassword.Text)
+        Try
+            Cnn.Open()
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString())
+        End Try
+        
+
+    End Sub
 End Class
