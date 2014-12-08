abae4024c47cad4d6732d1645280ecff8007f7d8
fkof <jesus120689@gmail.com>
2014-12-08 17:37:17 -0600

    lectura
    prueba de lectura de un archivo (.sql)

--- CompleteOP/CompleteOP/Form1.vb
+++ CompleteOP/CompleteOP/Form1.vb
@@ -1,14 +1,31 @@
-﻿Public Class Form1
+﻿Imports System.IO
+
+Public Class Form1
 
     Private Sub btnprocess_Click(sender As System.Object, e As System.EventArgs) Handles btnprocess.Click
         Dim Cnn As New SqlClient.SqlConnection
         Cnn.ConnectionString = String.Format("Server={0};Database={1}; User Id={2}; Password={3};", txtServidor.Text, txtbd.Text, txtusuario.Text, txtpassword.Text)
         Try
-            Cnn.Open()
+            ' Cnn.Open()
+
+            Dim lector As New StreamReader("C:\Users\ahernandez\Documents\Visual Studio 2010\Projects\SLAMNet\SQL_MANAGER\SP_SlamNet_Impo\SLAMNET_SPU_BORRARECIBO.sql")
+            While lector.Peek() <> -1
+                Dim linea As String = lector.ReadLine()
+                ' Si no está vacía, añadirla al control
+                ' Si está vacía, continuar el bucle
+                If String.IsNullOrEmpty(linea) Then
+                    Continue While
+                End If
+                RichTextBox1.Text = RichTextBox1.Text + linea + vbCrLf
+                'Aqui ya haces segun lo que deseas con la variable "linea"
+                'tu modificas a tu necesidad 
+            End While
+            ' Cerrar el fichero
+            lector.Close()
         Catch ex As Exception
             MsgBox(ex.Message.ToString())
         End Try
-        
+
 
     End Sub
 End Class
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nclientes, i, accidentes;
+		string nombre, clientes;
+		nclientes = 0;
+		nombre = "";
+		i = 0;
+		clientes = "";
+		accidentes = 0;
+		Console.WriteLine("Teclee la cantidad de clientes");
+		nclientes = int.Parse(Console.ReadLine());
+
+		while (i <= nclientes) 
+
+			{	Console.WriteLine("Teclee el nombre del cliente");
+				nombre = Console.ReadLine();
+				Console.WriteLine("Cuantos accidentes a tenido el cliente");
+				accidentes = int.Parse(Console.ReadLine());
+				if (accidentes >= 2 && accidentes <= 4)
+				{
+					clientes = clientes + nombre;
+
+				}
+					i = i + 1;
+				
+           	}
+        Console.WriteLine("Los clientes entre 2 y 4 accidentes son: " + clientes);
+        }
+    }
+}
