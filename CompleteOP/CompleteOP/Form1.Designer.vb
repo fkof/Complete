ad9c89c56c8d214d0e0dd422573f12d0d34178f1
fkof <jesus120689@gmail.com>
2014-12-19 15:54:22 -0600

    Cambio
    se puso boton para guardar el scrip en un archivo.

--- CompleteOP/CompleteOP/Form1.Designer.vb
+++ CompleteOP/CompleteOP/Form1.Designer.vb
@@ -23,27 +23,29 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.gbdatos = New System.Windows.Forms.GroupBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.txtServidor = New System.Windows.Forms.TextBox()
-        Me.txtusuario = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.txtpassword = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
-        Me.btnprocess = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.dtfecha = New System.Windows.Forms.DateTimePicker()
         Me.txtbd = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnprocess = New System.Windows.Forms.Button()
+        Me.txtpassword = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtusuario = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtServidor = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.dtfecha = New System.Windows.Forms.DateTimePicker()
-        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.gbdatos.SuspendLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'gbdatos
         '
+        Me.gbdatos.Controls.Add(Me.Button1)
         Me.gbdatos.Controls.Add(Me.Label7)
         Me.gbdatos.Controls.Add(Me.dtfecha)
         Me.gbdatos.Controls.Add(Me.txtbd)
@@ -62,62 +64,39 @@ Partial Class Form1
         Me.gbdatos.TabStop = False
         Me.gbdatos.Text = "GroupBox1"
         '
-        'Label1
+        'Label7
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(6, 30)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(46, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Servidor"
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(6, 198)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(102, 13)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Crear hasta la fecha"
         '
-        'txtServidor
+        'dtfecha
         '
-        Me.txtServidor.Location = New System.Drawing.Point(88, 23)
-        Me.txtServidor.Name = "txtServidor"
-        Me.txtServidor.Size = New System.Drawing.Size(192, 20)
-        Me.txtServidor.TabIndex = 1
+        Me.dtfecha.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtfecha.Location = New System.Drawing.Point(114, 191)
+        Me.dtfecha.Name = "dtfecha"
+        Me.dtfecha.Size = New System.Drawing.Size(166, 20)
+        Me.dtfecha.TabIndex = 9
         '
-        'txtusuario
+        'txtbd
         '
-        Me.txtusuario.Location = New System.Drawing.Point(88, 66)
-        Me.txtusuario.Name = "txtusuario"
-        Me.txtusuario.Size = New System.Drawing.Size(192, 20)
-        Me.txtusuario.TabIndex = 3
+        Me.txtbd.Location = New System.Drawing.Point(88, 145)
+        Me.txtbd.Name = "txtbd"
+        Me.txtbd.Size = New System.Drawing.Size(192, 20)
+        Me.txtbd.TabIndex = 8
+        Me.txtbd.Text = "Tfs_SlamNet"
         '
-        'Label2
+        'Label4
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(6, 73)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(43, 13)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Usuario"
-        '
-        'txtpassword
-        '
-        Me.txtpassword.Location = New System.Drawing.Point(88, 106)
-        Me.txtpassword.Name = "txtpassword"
-        Me.txtpassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.txtpassword.Size = New System.Drawing.Size(192, 20)
-        Me.txtpassword.TabIndex = 5
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(6, 113)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(53, 13)
-        Me.Label3.TabIndex = 4
-        Me.Label3.Text = "Password"
-        '
-        'RichTextBox1
-        '
-        Me.RichTextBox1.Location = New System.Drawing.Point(637, 35)
-        Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(425, 287)
-        Me.RichTextBox1.TabIndex = 1
-        Me.RichTextBox1.Text = ""
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 152)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Base de Datos"
         '
         'btnprocess
         '
@@ -128,21 +107,67 @@ Partial Class Form1
         Me.btnprocess.Text = "Button1"
         Me.btnprocess.UseVisualStyleBackColor = True
         '
-        'txtbd
+        'txtpassword
         '
-        Me.txtbd.Location = New System.Drawing.Point(88, 145)
-        Me.txtbd.Name = "txtbd"
-        Me.txtbd.Size = New System.Drawing.Size(192, 20)
-        Me.txtbd.TabIndex = 8
+        Me.txtpassword.Location = New System.Drawing.Point(88, 106)
+        Me.txtpassword.Name = "txtpassword"
+        Me.txtpassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtpassword.Size = New System.Drawing.Size(192, 20)
+        Me.txtpassword.TabIndex = 5
+        Me.txtpassword.Text = "Orbit#2014"
         '
-        'Label4
+        'Label3
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(6, 152)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(77, 13)
-        Me.Label4.TabIndex = 7
-        Me.Label4.Text = "Base de Datos"
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 113)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Password"
+        '
+        'txtusuario
+        '
+        Me.txtusuario.Location = New System.Drawing.Point(88, 66)
+        Me.txtusuario.Name = "txtusuario"
+        Me.txtusuario.Size = New System.Drawing.Size(192, 20)
+        Me.txtusuario.TabIndex = 3
+        Me.txtusuario.Text = "sa"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Usuario"
+        '
+        'txtServidor
+        '
+        Me.txtServidor.Location = New System.Drawing.Point(88, 23)
+        Me.txtServidor.Name = "txtServidor"
+        Me.txtServidor.Size = New System.Drawing.Size(192, 20)
+        Me.txtServidor.TabIndex = 1
+        Me.txtServidor.Text = "216.150.47.86"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Servidor"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox1.Location = New System.Drawing.Point(637, 35)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(425, 287)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
         '
         'DataGridView1
         '
@@ -170,22 +195,14 @@ Partial Class Form1
         Me.Label6.TabIndex = 10
         Me.Label6.Text = "Complete"
         '
-        'dtfecha
+        'Button1
         '
-        Me.dtfecha.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.dtfecha.Location = New System.Drawing.Point(114, 191)
-        Me.dtfecha.Name = "dtfecha"
-        Me.dtfecha.Size = New System.Drawing.Size(166, 20)
-        Me.dtfecha.TabIndex = 9
-        '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(6, 198)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(102, 13)
-        Me.Label7.TabIndex = 11
-        Me.Label7.Text = "Crear hasta la fecha"
+        Me.Button1.Location = New System.Drawing.Point(124, 265)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 39)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -222,5 +239,6 @@ Partial Class Form1
     Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
     Friend WithEvents Label5 As System.Windows.Forms.Label
     Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
--- CompleteOP/CompleteOP/Form1.vb
+++ CompleteOP/CompleteOP/Form1.vb
@@ -1,31 +1,147 @@
 ﻿Imports System.IO
+Imports System.Text
+Imports System.Text.RegularExpressions
 
 Public Class Form1
 
     Private Sub btnprocess_Click(sender As System.Object, e As System.EventArgs) Handles btnprocess.Click
-        Dim Cnn As New SqlClient.SqlConnection
-        Cnn.ConnectionString = String.Format("Server={0};Database={1}; User Id={2}; Password={3};", txtServidor.Text, txtbd.Text, txtusuario.Text, txtpassword.Text)
-        Try
-            ' Cnn.Open()
+        RichTextBox1.Text = String.Empty
+        DataGridView1.DataSource = Nothing
 
-            Dim lector As New StreamReader("C:\Users\ahernandez\Documents\Visual Studio 2010\Projects\SLAMNet\SQL_MANAGER\SP_SlamNet_Impo\SLAMNET_SPU_BORRARECIBO.sql")
-            While lector.Peek() <> -1
-                Dim linea As String = lector.ReadLine()
-                ' Si no está vacía, añadirla al control
-                ' Si está vacía, continuar el bucle
-                If String.IsNullOrEmpty(linea) Then
-                    Continue While
+        Dim Cnn As New SqlClient.SqlConnection
+        Cnn.ConnectionString = String.Format("Server={0};Database={1}; User Id={2}; Password={3};",
+                                             txtServidor.Text, txtbd.Text, txtusuario.Text, txtpassword.Text)
+        Try
+            Cnn.Open()
+            MsgBox(String.Format("{0:MM/dd/yyyy}", CDate(dtfecha.Text)))
+            Dim consulta As New StringBuilder
+            consulta.AppendLine("SELECT DISTINCT")
+            consulta.AppendLine("V.ChildItem")
+            consulta.AppendLine("FROM")
+            consulta.AppendLine("tbl_Changeset CS")
+            consulta.AppendLine("INNER JOIN tbl_Identity I ON I.IdentityID = CS.OwnerID")
+            consulta.AppendLine("INNER JOIN tbl_Version V ON V.VersionFrom = CS.ChangesetID")
+            consulta.AppendLine("INNER JOIN Constants C ON C.TeamFoundationId=I.TeamFoundationId")
+            consulta.AppendLine("WHERE ISNULL(CS.Comment,'') <> ''")
+            consulta.AppendLine("AND v.ParentPath like '$\SLAMNET\SLAMNet\SQL>MANAGER\SP>SlamNet>Impo\%'")
+            consulta.AppendLine("AND CS.CreationDate BETWEEN '" + String.Format("{0:MM/dd/yyyy}", CDate(dtfecha.Text)) + "'")
+            consulta.AppendLine("AND GETDATE()")
+
+
+
+            Dim datos As New Data.DataTable
+            Dim da As New SqlClient.SqlDataAdapter(consulta.ToString(), Cnn)
+            da.Fill(datos)
+            Cnn.Close()
+
+
+
+            For Each dr As Data.DataRow In datos.Rows
+                Dim archivo As String = dr(0)
+                archivo = archivo.Replace(">", "_")
+                archivo = archivo.Replace("\", "")
+                If File.Exists("C:\Users\ahernandez\Documents\Visual Studio 2010\Projects\SLAMNet\SQL_MANAGER\SP_SlamNet_Impo\" + archivo) Then
+
+
+                    Dim lector As New StreamReader("C:\Users\ahernandez\Documents\Visual Studio 2010\Projects\SLAMNet\SQL_MANAGER\SP_SlamNet_Impo\" + archivo)
+                    'While lector.Peek() <> -1
+                    '    Dim linea As String = lector.ReadLine()
+                    '    ' Si no está vacía, añadirla al control
+                    '    ' Si está vacía, continuar el bucle
+                    '    If String.IsNullOrEmpty(linea) Then
+                    '        Continue While
+                    '    End If
+                    RichTextBox1.Text = RichTextBox1.Text + lector.ReadToEnd.ToString() + vbCrLf
+                    '    'Aqui ya haces segun lo que deseas con la variable "linea"
+                    '    'tu modificas a tu necesidad 
+                    'End While
+                    ' Cerrar el fichero
+                    Dim separador As String = "--**********************************************************************************"
+                    RichTextBox1.Text = RichTextBox1.Text + separador + vbCrLf
+                    lector.Close()
+                Else
+                    Label6.Text = Label6.Text + " " + archivo
                 End If
-                RichTextBox1.Text = RichTextBox1.Text + linea + vbCrLf
-                'Aqui ya haces segun lo que deseas con la variable "linea"
-                'tu modificas a tu necesidad 
-            End While
-            ' Cerrar el fichero
-            lector.Close()
+            Next
+
+            DataGridView1.DataSource = datos
         Catch ex As Exception
             MsgBox(ex.Message.ToString())
         End Try
 
 
+    End Sub
+
+    Private Sub txtServidor_TextChanged(sender As System.Object, e As System.EventArgs) Handles txtServidor.TextChanged
+
+    End Sub
+
+    Private Sub Buscar_Coincidencia( _
+      ByVal pattern As String, _
+      ByVal RichTextBox As RichTextBox, _
+      ByVal cColor As Color, _
+      ByVal BackColor As Color)
+
+
+        Dim Resultados As MatchCollection
+        Dim Palabra As Match
+
+        Try
+            ' PAsar el pattern e indicar que ignore las mayúsculas y minúsculas al mosmento de buscar  
+            Dim obj_Expresion As New Regex(pattern.ToString, RegexOptions.IgnoreCase)
+
+            ' Ejecutar el método Matches para buscar la cadena en el texto del control  
+            ' y retornar un MatchCollection con los resultados  
+            Resultados = obj_Expresion.Matches(RichTextBox.Text)
+
+            ' quitar el coloreado anterior  
+            With RichTextBox
+                .SelectAll()
+                .SelectionColor = Color.Black
+            End With
+
+            ' Si se encontraron coincidencias recorre las colección    
+            For Each Palabra In Resultados
+                With RichTextBox
+                    .SelectionStart = Palabra.Index ' comienzo de la selección  
+                    .SelectionLength = Palabra.Length ' longitud de la cadena a seleccionar  
+                    .SelectionColor = cColor ' color de la selección  
+                    .SelectionBackColor = BackColor
+                    .Select(Palabra.Index, Palabra.Index)
+                    Debug.Print(Palabra.Value)
+
+                End With
+            Next Palabra
+
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
+
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        Dim texto As String
+        texto = DataGridView1.Rows(e.RowIndex).Cells(0).Value()
+        texto = texto.Replace(">", "_")
+        texto = texto.Replace("\", "")
+        texto = texto.Replace(".sql", "")
+
+        '    Buscar_Coincidencia(texto, RichTextBox1, Color.Blue, Color.Yellow)
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim saveFile1 As New SaveFileDialog()
+
+        ' Initialize the SaveFileDialog to specify the RTF extension for the file.
+        saveFile1.DefaultExt = "*.sql"
+        saveFile1.Filter = "sql|*.sql"
+
+        ' Determine if the user selected a file name from the saveFileDialog. 
+        If (saveFile1.ShowDialog() = System.Windows.Forms.DialogResult.OK) And (saveFile1.FileName.Length) > 0 Then
+
+            ' Save the contents of the RichTextBox into the file.
+            RichTextBox1.SaveFile(saveFile1.FileName, _
+                RichTextBoxStreamType.PlainText)
+        End If
     End Sub
 End Class
